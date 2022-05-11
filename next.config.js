1b37674e32643ee5e755b8c4e2f08f37073fc25b
vu <duyvujarvan4@gmail.com>
2022-05-11 12:05:32 +0700

    update

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ module.exports = withImages({
   reactStrictMode: true,
   images: {
     disableStaticImages: true,
-    domains: [process.env.NEXT_PUBLIC_IMAGE_URL, "cf.shopee.vn"],
+    domains: ["demo.satavan.com", "cf.shopee.vn"],
   },
   i18n,
 })
